e7c9bdf3cc084835fdf79c2f5467efbbbd9d8db0
Bilgehan KALKAN <bilgehan.kalkan@consultant.turkcell.com.tr>
2019-03-08 15:44:12 +0300

    TBaseApplication created, multidex added.

---
+++ app/src/main/java/com/turkcell/tbase/view/core/TBaseApplication.kt
@@ -0,0 +1,12 @@
+package com.turkcell.tbase.view.core
+
+import androidx.multidex.MultiDexApplication
+
+class TBaseApplication: MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
