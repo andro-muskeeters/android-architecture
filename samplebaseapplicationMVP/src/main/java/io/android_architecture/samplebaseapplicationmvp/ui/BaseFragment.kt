0c8a65864b4b0852f10d10cdb879d8b53afe075d
Bilgehan KALKAN <bilgehan.kalkan@consultant.turkcell.com.tr>
2019-03-14 10:09:50 +0300

    Fragment Navigation implemented and BaseActivity removed.

--- samplebaseapplicationMVP/src/main/java/io/android_architecture/samplebaseapplicationmvp/ui/BaseFragment.kt
+++ samplebaseapplicationMVP/src/main/java/io/android_architecture/samplebaseapplicationmvp/ui/BaseFragment.kt
@@ -7,8 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 abstract class BaseFragment : Fragment() {
+
+    val navController by lazy {
+        Navigation.findNavController(activity as FragmentActivity, navigation_host.id)
+    }
 
     //Called when Fragment's view created.
     abstract fun createUI(rootView: View, savedInstanceState: Bundle?)
---
+++ samplebaseapplicationMVP/src/main/java/io/android_architecture/samplebaseapplicationmvp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.android_architecture.samplebaseapplicationmvp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.android_architecture.samplebaseapplicationmvp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
