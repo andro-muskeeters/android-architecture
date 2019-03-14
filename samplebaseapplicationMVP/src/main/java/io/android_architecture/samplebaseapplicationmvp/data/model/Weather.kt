f6b3bc3f3dbbe0a0f181939ab944a9d81cfe2442
Bilgehan KALKAN <bilgehan.kalkan@consultant.turkcell.com.tr>
2019-03-14 13:32:54 +0300

    Pull from sample-mvp.

--- samplebaseapplicationMVP/src/main/java/io/android_architecture/samplebaseapplicationmvp/data/model/Weather.kt
+++
@@ -1,8 +0,0 @@
-package io.android_architecture.samplebaseapplicationmvp.data.model
-
-class Weather(
-    val id: Int,
-    val main: String,
-    val description: String,
-    val icon: String
-)
--- samplebaseapplicationMVP/src/main/java/io/android_architecture/samplebaseapplicationmvp/data/model/Wind.kt
+++
@@ -1,6 +0,0 @@
-package io.android_architecture.samplebaseapplicationmvp.data.model
-
-data class Wind(
-    val speed: Double,
-    val deg: Int
-)
