34e1c5cd05da2bbb86cb47b20552093f52548278
Bilgehan KALKAN <bilgehan.kalkan@consultant.turkcell.com.tr>
2019-04-05 09:45:49 +0300

    Glide module added.

---
+++ base/src/main/java/io/android_architecture/base/core/util/module/GlideAppModule.kt
@@ -0,0 +1,7 @@
+package io.android_architecture.base.core.util.module
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideAppModule : AppGlideModule()
